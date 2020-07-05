1b50a931069292a959e15a0abaa20ed5286d95ed
Akhilesh <akhil662@gmail.com>
2020-07-05 22:16:33 +0530

    sunday

--- day27.py
+++ day27.py
@@ -6,3 +6,30 @@ For example, given the string "([])[]({})", you should return true.
 Given the string "([)]" or "((()", you should return false.
 
 '''
+
+string = "([])[]({})"
+
+
+
+def check(string):
+    print(string)
+    stack = []
+    maps = {"}":"{","]":"[",")":"("}
+    for i in range(len(string)):
+        if string[i] in ("]",")","}"):
+            if stack:
+                popped = stack.pop()
+                if maps[string[i]]!= popped:
+                    return False
+            else:
+                return False
+        else:
+            stack.append(string[i])
+    return len(stack)==0
+
+
+print(check(string))
+print(check("(([])[]({}))"))
+print(check("([]){}"))
+print(check("([]})"))
+print(check("([)]"))
